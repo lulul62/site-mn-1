d3313ca14b2c85368a69f9dd5f98232714e6d76d
Yves62 <78845901+Yves62@users.noreply.github.com>
2021-03-30 11:40:58 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -264,6 +264,19 @@
 
     </section>
 
+    <section id="localisation">
+    
+        <div>
+            <h5>Où me trouver</h5>
+
+            <div class="image_localisation">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5099.8678372538225!2d2.78233093458399!3d50.27449245407036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dd488750ed0845%3A0xc0b6272cedb94794!2s20%20Rue%20Victor%20Delaporte%2C%2062217%20Achicourt!5e0!3m2!1sfr!2sfr!4v1617091972134!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        
+        </div>
+    
+    </section>
+
     
     
     <footer>
